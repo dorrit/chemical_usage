37f665dfa56695a30abd05c0956d2bad205a67c7
dorrit <dorritgeshuri@gmail.com>
2014-02-02 13:58:29 -0800

    create chemical class and specs

--- lib/chemical.rb
+++ lib/chemical.rb
@@ -1,6 +1,13 @@
 class Chemical
 
-
-
+	def initialize(name, initial_quantity)
+			@name = name
+			@initial_quantity = initial_quantity
+	end
 
 end
+
+
+
+
+ 
--- spec/chemical_spec.rb
+++ spec/chemical_spec.rb
@@ -2,5 +2,11 @@ require 'rspec'
 require 'chemical'
 
 describe Chemical do 
+	context 'initialization' do
+		it 'initializes an instance of Chemical with all its attributes' do
+			chemical = Chemical.new('sodium chloride', 13)
+			chemical.should be_an_instance_of Chemical
+		end
+	end
+end 
 
-end
